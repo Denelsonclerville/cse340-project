c9fa57aee091e1a4d36db3908c5b4edd947c1c6c
Denelsonclerville <denelsonclerville8@gmail.com>
2026-09-11 09:53:55 -0400

    add

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const __dirname = path.dirname(__filename);
 const app = express();
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "src", "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
