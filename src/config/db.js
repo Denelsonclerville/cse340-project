2cc56e8cd7802ec216921d3a2a76fbf7ba10258b
Denelsonclerville <denelsonclerville8@gmail.com>
2026-09-23 09:44:26 -0400

    add

--- src/config/db.js
+++ src/config/db.js
@@ -2,15 +2,17 @@ import "dotenv/config";
 import pg from "pg";
 
 const { Pool } = pg;
+
+const hasRenderDb = process.env.DATABASE_URL?.includes("render.com");
 const isProduction = process.env.NODE_ENV?.toLowerCase() === "production";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: isProduction ? { rejectUnauthorized: false } : false,
+  ssl: isProduction || hasRenderDb ? { rejectUnauthorized: false } : false,
 });
 
 pool.on("error", (error) => {
   console.error("Unexpected PostgreSQL pool error:", error);
 });
 
-export default pool;
+export default pool
